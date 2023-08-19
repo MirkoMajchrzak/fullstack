6301108fb6a1d0bd963f9603ac3b85140f5d48c5
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2023-08-19 07:55:07 +0200

    added Libary and SidebarItems

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,8 @@ import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
+import Libary from "./Libary";
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -46,9 +48,22 @@ const Sidebar: React.FC<SidebarProps> = ({
                 "
             >
                 <Box>
-                    Sidebar Navigation
+                    <div className="flex flex-col gap-y-4 px-5 py-4">
+                        {routes.map((item) =>(
+                            <SidebarItem
+                            key={item.label}
+                            {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className="overflow-y-auto h-full">
+                    <Libary />
                 </Box>
             </div>
+            <main className="h-full flex-1 overflow-y-auto py-2">
+                {children}
+            </main>
         </div>
     );
 }
