87c5b08926551ab9c03b6c47a7f08853a21e049b
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2023-08-20 17:35:58 +0200

    liked.png added

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,8 +1,29 @@
-const ListItem = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+import React from "react";
+
+interface ListItemProps {
+    image: string;
+    name: string;
+    href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) => {
+    const router = useRouter();
+
+    const onClick = () => {
+        //Addd authentification before Push
+        router.push(href);
+    }
     return ( 
-        <div className="">
-            List Item!
-        </div>
+        <button className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4">
+
+        </button>
      );
 }
  
