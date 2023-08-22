08a0288a7dbf0c03f6cf3a46631e36a0ff36c42d
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2023-08-22 19:51:24 +0200

    Upload Function finidshed

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -9,11 +9,15 @@ import Button from "./Button";
 import toast from "react-hot-toast";
 import { useUser } from "@/hooks/useUser";
 import uniqid from "uniqid";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
 
 const UploadModal = () => {
     const [isLoading, setIsLoading] = useState(false);
     const uploadModal = useUploadModal();
     const { user } = useUser();
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
 
     const {
         register,
@@ -48,6 +52,65 @@ const UploadModal = () => {
             }
 
             const uniqueId = uniqid();
+
+            //Upload Songs
+
+            const {
+                data: songData,
+                error: songError,
+            } = await supabaseClient
+            .storage
+            .from('songs')
+            .upload(`song-${values.title}-${uniqueId}`, songFile, {
+                cacheControl: '3600',
+                upsert: false
+            });
+
+            if (songError) {
+                setIsLoading(false);
+                return toast.error('Song Upload fehlgeschlagen');
+            }
+
+            // Upload Image
+
+            const {
+                data: imageData,
+                error: imageError,
+            } = await supabaseClient
+            .storage
+            .from('images')
+            .upload(`image-${values.title}-${uniqueId}`, imageFile, {
+                cacheControl: '3600',
+                upsert: false
+            });
+
+            if (imageError) {
+                setIsLoading(false);
+                return toast.error('Bild Upload fehlgeschlagen');
+            }
+
+            const {
+                error: supabaseError
+            } = await supabaseClient
+            .from('songs')
+            .insert({
+                user_id: user.id,
+                title: values.title,
+                author: values.author,
+                image_path: imageData.path,
+                song_path: songData.path
+            });
+
+            if (supabaseError) {
+                setIsLoading(false);
+                return toast.error(supabaseError.message);
+            }
+
+            router.refresh();
+            setIsLoading(false);
+            toast.success('Song hochgeladen!')
+            reset();
+            uploadModal.onClose();
 
         } catch (error) {
             toast.error("Irgendwas ist schief gelaufen");
