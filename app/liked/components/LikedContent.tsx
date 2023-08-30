8c9ab38e896ceffba3962b2f04ea1b4a0b447d93
MirkoMajchrzak <mirkomajchrzak@hotmail.com>
2023-08-30 17:01:34 +0200

    Like Function added

--- app/liked/components/LikedContent.tsx
+++ app/liked/components/LikedContent.tsx
@@ -2,26 +2,38 @@
 
 import LikeButton from "@/components/Like.Button";
 import MediaItem from "@/components/MediaItems";
+import { useUser } from "@/hooks/useUser";
 import { Song } from "@/types";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
 
-interface SearchContentProps {
+interface LikedContentProps {
     songs: Song[];
 }
 
-const SearchContent: React.FC<SearchContentProps> = ({
+const LikedContent: React.FC<LikedContentProps> = ({
     songs
 }) => {
+    const router = useRouter();
+    const { isLoading, user } = useUser();
+
+    useEffect (() => {
+        if (!isLoading && !user) {
+            router.replace('/');
+        }
+    }, [isLoading, user, router]);
+
+
     if (songs.length === 0) {
         return (
             <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
-                Nix Musik da!
+                Keine gelikten Songs
             </div>
         )
     }
 
     return ( 
-        <div className="flex flex-col gap-y-2 w-full px-6">
+        <div className="flex flex-col gap-y-2 w-full p-6">
             {songs.map((song) => (
                 <div key={song.id} className="flex items-center gap-x-4 w-full">
                     <div className="flex-1">
@@ -31,10 +43,11 @@ const SearchContent: React.FC<SearchContentProps> = ({
                         />
                     </div>
                     <LikeButton songId={song.id} />
+
                 </div>
             ))}
         </div>
      );
 }
  
-export default SearchContent;
+export default LikedContent;
